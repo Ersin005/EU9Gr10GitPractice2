df94622029474bf0001c31f06ba6a95399ae9432
Ersin KAYHAN <dekked2438@gmail.com>
2022-07-27 17:41:31 +0300

    Second Trial...

---
+++ src/ErsTest/Group10SelfStudy.java
@@ -0,0 +1,19 @@
+package ErsTest;
+
+public class Group10SelfStudy {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Group_10, how are you today?");
+
+        //I started to use GIT.
+
+
+
+
+
+    }
+
+
+
+
+}
